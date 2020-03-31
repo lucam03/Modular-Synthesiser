4be2530ca57c624890292a96fb83d6f3f45a0982
lucam03 <62672581+lucam03@users.noreply.github.com>
2020-03-31 22:16:07 +0100

    Removed pureknob, can now create and use multiple oscillators at once

--- osc.js
+++ osc.js
@@ -1,41 +1,61 @@
 var context = new AudioContext();
-var oscKnobs = document.getElementById("oscKnobs");
-var oscArray = []
-//TEST COMMENT PUSH
+var oscSliders = document.getElementById("oscSliders");
+var oscArray = [];
 
 class Osc {
 	constructor() {
-		this.osc = context.createOscillator()
+		Osc.numInstances = (Osc.numInstances + 1 || 0)
+		this.osc = context.createOscillator();
+		this.osc.started = false;
 		this.osc.frequency.value = 262;
-		this.oscKnob = pureknob.createKnob(300, 300);
-		this.oscKnob.setProperty("valMax", 4186);
-		this.oscKnob.setProperty("valMin", 28);
-		this.oscKnob.setValue(262);
-		this.knobListener = function(knob, value) {
-			console.log(value);
-			this.osc.frequency.value = value;
-		}
-		this.oscKnob.addListener(this.knobListener);
-		this.oscNode = this.oscKnob.node();
-		this.oscElement = document.createElement("div");
-		this.oscElement.appendChild(this.oscNode);
-		oscKnobs.appendChild(this.oscElement);
+		this.oscSlider = document.createElement("input");
+		this.oscSlider.id = `slider${Osc.numInstances}`
+		this.oscSlider.type = "range";
+		this.oscSlider.min = 28;
+		this.oscSlider.max = 4186;
+		this.oscSlider.value = 262;
+		oscSliders.appendChild(this.oscSlider);
+		this.playButton = document.createElement("button");
+		oscSliders.appendChild(this.playButton);
+		this.playButton.innerHTML = "PLAY"
+		this.playButton.id = `play${Osc.numInstances}`
+		this.stopButton = document.createElement("button");
+		this.stopButton.innerHTML = "STOP"
+		this.stopButton.id = `stop${Osc.numInstances}`
+		oscSliders.appendChild(this.stopButton);
+		console.log(oscSliders);
 	}
 	playOsc() {
+		this.osc.connected = true;
 		this.osc.connect(context.destination);
 		this.osc.type = "sine";
-		this.osc.started = false;
-		this.osc.frequency.value = thisoscKnob.getValue();
-		if (this.osc.started === false) {
+		this.osc.frequency.value = this.oscSlider.value;
+		if (!this.osc.started) {
 			this.osc.start();
 			this.osc.started = true;
 		}
+		console.log("Started");
 	}
 	stopOsc() {
+		if (this.osc.connected) {
 			this.osc.disconnect(context.destination);
+			this.osc.connected = false;
+			console.log("Stopped");
+		}
   }
+	updateFrequency() {
+		this.osc.frequency.value = this.oscSlider.value;
+		this.playButton.innerHTML = this.oscSlider.value;
+		console.log(this.oscSlider.value);
+	}
 }
 
 function createOsc() {
 	oscArray.push(new Osc);
+	var newPlayButton = document.getElementById(`play${Osc.numInstances}`);
+	var newStopButton = document.getElementById(`stop${Osc.numInstances}`);
+	var newSlider = document.getElementById(`slider${Osc.numInstances}`);
+	newSlider.oninput = eval(`(function() {oscArray[${parseInt(Osc.numInstances.toString())}].updateFrequency()})`);
+	newPlayButton.onclick = eval(`(function() {oscArray[${parseInt(Osc.numInstances.toString())}].playOsc()})`);
+	newStopButton.onclick = eval(`(function() {oscArray[${parseInt(Osc.numInstances.toString())}].stopOsc()})`);
 }
